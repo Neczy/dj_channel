49b07261f69a0ce9b884ff21e1b2e856138354ba
Arthur <asad.artur@gmail.com>
2020-09-23 21:08:26 +0300

    sync to async, add tests

--- st_chat/st_chat/settings.py
+++ st_chat/st_chat/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from .static import token
 
+import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -73,7 +75,10 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
